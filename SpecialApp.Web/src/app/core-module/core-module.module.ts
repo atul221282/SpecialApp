c13144643fab924714eea3958673a6d9c467d5ae
Atul Chaudhary <atul221282@gmail.com>
2017-03-16 21:44:15 +1030

    got angular2 with ngmodule

---
+++ SpecialApp.Web/src/app/core-module/core-module.module.ts
@@ -0,0 +1,13 @@
+﻿import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoadingSpinnerComponent } from './';
+
+@NgModule({
+    imports: [
+        CommonModule
+    ],
+    declarations: [
+        LoadingSpinnerComponent
+    ]
+})
+export class CoreModuleModule { }
