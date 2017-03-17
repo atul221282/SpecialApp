2ce1210991bb2faccdade525362c07f50b4dc0c2
Atul Chaudhary <atul221282@gmail.com>
2017-03-18 00:31:20 +1030

    got angular material working

--- SpecialApp.Web/src/app/app.module.ts
+++ SpecialApp.Web/src/app/app.module.ts
@@ -2,6 +2,7 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { MaterialModule } from '@angular/material';
 import { AppComponent } from './app.component';
 import { CoreModule } from './';
 @NgModule({
@@ -12,6 +13,7 @@ import { CoreModule } from './';
         BrowserModule,
         FormsModule,
         HttpModule,
+        MaterialModule,
         CoreModule
     ],
     providers: [],
