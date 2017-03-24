1593cf76f9cd719f6425c264df09528c53b78c79
Atul Chaudhary <atul221282@gmail.com>
2017-03-25 01:40:39 +1030

    some more changes

---
+++ SpecialApp.Database/Location.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[SpecialLocation]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Location] [sys].[geography] NOT NULL, 
+    [AuditCreatedDate] DATETIMEOFFSET NOT NULL, 
+    [AuditLastUpdatedDate] DATETIMEOFFSET NOT NULL, 
+    [AuditLastUpdatedBy] DATETIMEOFFSET NOT NULL, 
+    [AuditCreatedBy] VARCHAR(100) NOT NULL 
+)
