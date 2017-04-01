2790065bffec3e19f0a07d0c5099ead879581e8e
Atul Chaudhary <atul221282@gmail.com>
2017-04-01 22:28:31 +1030

    created account module

---
+++ SpecialApp.Web/src/app/account-module/account.module.ts
@@ -0,0 +1,16 @@
+﻿import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ForgotPasswordComponent } from './';
+@NgModule({
+    imports: [
+
+    ],
+    declarations: [
+        ForgotPasswordComponent
+    ],
+    providers: [
+        
+    ],
+    bootstrap: [ForgotPasswordComponent]
+})
+export class AccountModule { }
---
+++ SpecialApp.Web/src/app/account-module/password/forgot-password.component.ts
@@ -0,0 +1,16 @@
+﻿import { Component, OnInit, OnChanges } from '@angular/core';
+
+@Component({
+    selector: 'forgot-password',
+    templateUrl: './forgot-password.component.html',
+    styleUrls: ['./forgot-password.component.scss']
+})
+export class ForgotPasswordComponent implements OnInit, OnChanges {
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+    ngOnChanges() {
+    }
+}
