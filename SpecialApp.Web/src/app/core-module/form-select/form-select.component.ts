b2ee56a57dcb8cddaa7effb01a233a9b8d37f52b
Atul Chaudhary <atul221282@gmail.com>
2017-04-09 23:46:25 +0930

    got autocomplete working with hack stuff

--- SpecialApp.Web/src/app/core-module/form-select/form-select.component.ts
+++ SpecialApp.Web/src/app/core-module/form-select/form-select.component.ts
@@ -5,12 +5,12 @@ import { FormGroup, FormControl } from '@angular/forms';
     templateUrl: './form-select.component.html',
     styleUrls: ['./form-select.component.css']
 })
-export class FormSelectComponent {
-    //@Input() list: Array<any>;
-    //@Input() spTextField: string;
-    //@Input() spValueField: string;
-    //@Input() form: FormGroup;
-    //@Input() property: string;
+export class FormSelectComponent implements OnInit {
+    @Input() list: Array<any>;
+    @Input() spTextField: string;
+    @Input() spValueField: string;
+    @Input() form: FormGroup;
+    @Input() property: string;
 
     //selectedValue: string;
     //constructor() { }
@@ -19,71 +19,49 @@ export class FormSelectComponent {
     //}
 
     stateCtrl: FormControl;
+    idControl: FormControl;
     filteredStates: any;
-
-    states = [
-        'Alabama',
-        'Alaska',
-        'Arizona',
-        'Arkansas',
-        'California',
-        'Colorado',
-        'Connecticut',
-        'Delaware',
-        'Florida',
-        'Georgia',
-        'Hawaii',
-        'Idaho',
-        'Illinois',
-        'Indiana',
-        'Iowa',
-        'Kansas',
-        'Kentucky',
-        'Louisiana',
-        'Maine',
-        'Maryland',
-        'Massachusetts',
-        'Michigan',
-        'Minnesota',
-        'Mississippi',
-        'Missouri',
-        'Montana',
-        'Nebraska',
-        'Nevada',
-        'New Hampshire',
-        'New Jersey',
-        'New Mexico',
-        'New York',
-        'North Carolina',
-        'North Dakota',
-        'Ohio',
-        'Oklahoma',
-        'Oregon',
-        'Pennsylvania',
-        'Rhode Island',
-        'South Carolina',
-        'South Dakota',
-        'Tennessee',
-        'Texas',
-        'Utah',
-        'Vermont',
-        'Virginia',
-        'Washington',
-        'West Virginia',
-        'Wisconsin',
-        'Wyoming',
-    ];
+    stateValue: string;
 
     constructor() {
         this.stateCtrl = new FormControl();
+        this.idControl = new FormControl();
+        
+    }
+    ngOnInit() {
+        this.stateValue = undefined;
         this.filteredStates = this.stateCtrl.valueChanges
             .startWith(null)
             .map(name => this.filterStates(name));
+        this.stateCtrl.valueChanges.subscribe(value => this.setForm(this.stateCtrl, this.form));
     }
 
     filterStates(val: string) {
-        return val ? this.states.filter(s => new RegExp(`^${val}`, 'gi').test(s))
-            : this.states;
+
+        var lis= val ? this.list.filter(s => new RegExp(`^${val}`, 'gi').test(s[this.spTextField]))
+            : this.list;
+
+        return lis;
+    }
+    getValue(c: FormControl) {
+
+    }
+    setForm(c: FormControl, form: FormGroup) {
+        if (c.value && c.value !== null) {
+            if (c.value[this.spValueField] && c.value[this.spValueField] !== null) {
+                this.stateValue = c.value[this.spTextField];
+                this.form.get(this.property).setValue(c.value.value);
+            }
+            else {
+                this.form.get(this.property).setValue(null);
+                this.stateValue = "";
+            }
+        }
+        else {
+            this.stateValue = "";
+            this.form.get(this.property).setValue(null);
+        }
+        
     }
 
 }
