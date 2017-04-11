8a483611851c43d25d882672584f1a5fb8e3a5b1
atul <atul221282@gmail.com>
2017-04-11 15:49:38 +0930

    got autocomplete working

--- SpecialApp.Web/src/app/core-module/form-select/form-select.component.ts
+++ SpecialApp.Web/src/app/core-module/form-select/form-select.component.ts
@@ -12,24 +12,14 @@ export class FormSelectComponent implements OnInit {
     @Input() form: FormGroup;
     @Input() property: string;
 
-    //selectedValue: string;
-    //constructor() { }
-
-    //ngOnInit() {
-    //}
-
     stateCtrl: FormControl;
-    idControl: FormControl;
     filteredStates: any;
-    stateValue: string;
 
     constructor() {
         this.stateCtrl = new FormControl();
-        this.idControl = new FormControl();
-        
     }
+
     ngOnInit() {
-        this.stateValue = undefined;
         this.filteredStates = this.stateCtrl.valueChanges
             .startWith(null)
             .map(name => this.filterStates(name));
@@ -37,31 +27,20 @@ export class FormSelectComponent implements OnInit {
     }
 
     filterStates(val: string) {
-
-        var lis= val ? this.list.filter(s => new RegExp(`^${val}`, 'gi').test(s[this.spTextField]))
-            : this.list;
-
-        return lis;
+        return this.list;
     }
-    getValue(c: FormControl) {
 
-    }
     setForm(c: FormControl, form: FormGroup) {
+        this.form.get(this.property).setValue(null);
         if (c.value && c.value !== null) {
             if (c.value[this.spValueField] && c.value[this.spValueField] !== null) {
-                this.stateValue = c.value[this.spTextField];
                 this.form.get(this.property).setValue(c.value.value);
             }
-            else {
-                this.form.get(this.property).setValue(null);
-                this.stateValue = "";
-            }
         }
-        else {
-            this.stateValue = "";
-            this.form.get(this.property).setValue(null);
-        }
-        
+    }
+
+    displayFn(data: any) {
+        return data ? data[this.spTextField] : data;
     }
 
 }
