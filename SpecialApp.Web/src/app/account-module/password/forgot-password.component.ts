17ba1afa01d9633c5486a385fed69fe77b62866f
Atul Chaudhary <atul221282@gmail.com>
2017-04-13 00:09:21 +0930

    some more changes

--- SpecialApp.Web/src/app/account-module/password/forgot-password.component.ts
+++ SpecialApp.Web/src/app/account-module/password/forgot-password.component.ts
@@ -1,16 +1,28 @@
-﻿import { Component, OnInit, OnChanges } from '@angular/core';
+﻿import { Component, OnInit, Inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
     selector: 'forgot-password',
     templateUrl: './forgot-password.component.html',
     styleUrls: ['./forgot-password.component.scss']
 })
-export class ForgotPasswordComponent implements OnInit, OnChanges {
-    constructor() { }
+export class ForgotPasswordComponent implements OnInit {
+    public forgotPasswordForm: FormGroup;
+    public emailErrors = {
+        required: "Email address is required",
+        minlength: "Emaill address can't be less than 5 characters",
+        maxlength: "Emaill address can't be greater than 50 characters",
+        email: "Invalid email"
+    }
+    constructor(private _fb: FormBuilder) { }
 
     ngOnInit() {
-    }
-
-    ngOnChanges() {
+        this.forgotPasswordForm = this._fb.group({
+            EmailAddress: [{ value: '', disabled: false }, [
+                Validators.required,
+                Validators.minLength(5),
+                Validators.maxLength(50),
+            ]]
+        });
     }
 }
