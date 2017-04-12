e4a30a84099d981deadee41a5fd1a56862339a86
Atul Chaudhary <atul221282@gmail.com>
2017-04-12 22:33:15 +0930

    applied date format

--- SpecialApp.Web/src/app/account-module/register-customer/register-customer.component.ts
+++ SpecialApp.Web/src/app/account-module/register-customer/register-customer.component.ts
@@ -30,6 +30,17 @@ export class RegisterCustomerComponent implements OnInit {
     public foodTypeMessage = {
         required: 'Food type is required'
     };
+
+    public phoneMessage = {
+        required: 'Phone number is required'
+    };
+
+    public dobMessage = {
+        required: 'DOB is required',
+        maxlength: 'Invalid DOB',
+        pattern:'Invalid DOB fromat'
+    };
+
     public confirmPasswordMessage = {
         required: 'Confirm password is required',
         match: 'Password do not match'
@@ -70,7 +81,11 @@ export class RegisterCustomerComponent implements OnInit {
                 ConfirmPassword: ['', Validators.required],
             }, { validator: passwordMatcher }),
             FoodType: [null, Validators.required],
-            PhoneNumber: ''
+            PhoneNumber: ['', Validators.required],
+            DateOfBirth: ['', [
+                Validators.required,
+                Validators.maxLength(10),
+                Validators.pattern(/^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[1,3-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/)]]
         });
     }
 
--- SpecialApp.Web/src/app/core-module/form-date/form-date.component.ts
+++ SpecialApp.Web/src/app/core-module/form-date/form-date.component.ts
@@ -1,15 +1,67 @@
-﻿import { Component, OnInit } from '@angular/core';
+﻿import { Component, OnInit, Input } from '@angular/core';
+import { FormGroup, AbstractControl } from '@angular/forms';
 
 @Component({
-  selector: 'form-date',
-  templateUrl: './form-date.component.html',
-  styleUrls: ['./form-date.component.css']
+    selector: 'form-date',
+    templateUrl: './form-date.component.html',
+    styleUrls: ['./form-date.component.css']
 })
 export class FormDateComponent implements OnInit {
 
-  constructor() { }
+    @Input() form: FormGroup;
+    @Input() property: string;
+    @Input() spPlaceholder: string;
+    @Input() spRequired: boolean;
+    @Input() validationMessages: any;
 
-  ngOnInit() {
-  }
+    control: AbstractControl;
+    errorMessages: string;
+    modelValue: string;
 
+    constructor() { }
+
+    ngOnInit() {
+        this.control = this.form.get(this.property);
+        this.control.valueChanges.subscribe(value => this.setMessage(this.control));
+        this.form.valueChanges.subscribe(value => this.setFormMessage(this.form));
+    }
+
+    setMessage(c: AbstractControl): void {
+        this.setDateValue(c);
+
+        if (!this.validationMessages) return;
+        this.errorMessages = '';
+        if ((c.touched || c.dirty) && c.errors) {
+            this.errorMessages = Object.keys(c.errors).map(key => this.validationMessages[key]).join(', ');
+        }
+    }
+
+    private setDateValue(c: AbstractControl) {
+        if (!c.value || c.value === null || c.value === '')
+            return;
+    }
+
+    setFormMessage(formGroup: FormGroup) {
+        if (!this.validationMessages) return;
+        //if child got an error keep that and return
+        if (formGroup.get(this.property).errors)
+            return;
+        this.errorMessages = '';
+        if ((formGroup.touched || formGroup.dirty) && formGroup.errors) {
+            this.errorMessages = Object.keys(formGroup.errors).map(key => this.validationMessages[key]).join(', ');
+        }
+    }
+
+    OnFocus() {
+        this.spPlaceholder = 'DOB';
+    }
+
+    OnFocusOut() {
+        this.spPlaceholder = "DOB dd/mm/yyyy";
+    }
+
+    OnBlur() {
+        let ff = this.control.value;
+        debugger;
+    }
 }
