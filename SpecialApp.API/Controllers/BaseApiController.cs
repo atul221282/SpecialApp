ad8681f8d157ab8e38419629ab3adf1f1756c136
Atul Chaudhary <atul221282@gmail.com>
2017-04-14 00:01:29 +0930

    api controller checkin

--- SpecialApp.API/Controllers/BaseApiController.cs
+++ SpecialApp.API/Controllers/BaseApiController.cs
@@ -5,7 +5,7 @@ using SpecialApp.API.Helpers;
 namespace SpecialApp.API.Controllers
 {
     [Route("api/[controller]")]
-    [Produces("application/json")]
+    //[Produces("application/json")]
     [EnableCors(APIGlobalConstants.CorsPolicy)]
     public abstract class BaseApiController : Controller
     {
