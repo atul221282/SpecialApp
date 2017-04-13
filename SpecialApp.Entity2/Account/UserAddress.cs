2c8688a7be7bc1fb9767e9241cf1e1491bca0137
Atul Chaudhary <atul221282@gmail.com>
2017-04-13 21:39:53 +0930

    some more changes

--- SpecialApp.Entity2/Account/UserAddress.cs
+++ SpecialApp.Entity2/Account/UserAddress.cs
@@ -7,8 +7,11 @@ namespace SpecialApp.Entity.Account
     public class UserAddress
     {
         public int UserId { get; set; }
+
         public Users Users { get; set; }
+
         public int AddressId { get; set; }
+
         public Address Address { get; set; }
     }
 }
