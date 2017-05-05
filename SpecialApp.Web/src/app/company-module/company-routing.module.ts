ecb3d910432458994e93f2bc14795d71b1f2e3d8
Atul Chaudhary <atul221282@gmail.com>
2017-05-05 21:17:02 +0930

    created company module

---
+++ SpecialApp.Web/src/app/company-module/company-routing.module.ts
@@ -0,0 +1,23 @@
+﻿import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+    {
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+
+export class AccountRouterModule { }
+
+export const routedComponents = [
+];
+
+/*
+Copyright 2016 JohnPapa.net, LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://bit.ly/l1cense
+*/
---
+++ SpecialApp.Web/src/app/company-module/company.module.ts
@@ -0,0 +1,22 @@
+﻿import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from '../core-module/core.module';
+
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MaterialModule,
+        FlexLayoutModule,
+        BrowserAnimationsModule,
+        CoreModule
+    ],
+    declarations: []
+})
+export class CompanyModule { }
