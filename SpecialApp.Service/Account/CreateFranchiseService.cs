bc11583fa8049926c1a13e9e2f20ee513785f655
Atul Chaudhary <atul221282@gmail.com>
2017-05-14 22:05:25 +0930

    working on creating franchise service

---
+++ SpecialApp.Service/Account/CreateFranchiseService.cs
@@ -0,0 +1,24 @@
+﻿using SpecialApp.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using SpecialApp.Entity.Model.Account;
+using System.Threading.Tasks;
+
+namespace SpecialApp.Service.Account
+{
+    public class CreateFranchiseService : ICreateFranchiseService
+    {
+        private readonly ISpecialUOW uow;
+
+        public CreateFranchiseService(ISpecialUOW uow)
+        {
+            this.uow = uow;
+        }
+
+        public async Task Create(CreateFranchiseModel model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ SpecialApp.Service/Account/ICreateFranchiseService.cs
@@ -0,0 +1,13 @@
+﻿using SpecialApp.Entity.Model.Account;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpecialApp.Service.Account
+{
+    public interface ICreateFranchiseService
+    {
+        Task Create(CreateFranchiseModel model);
+    }
+}
