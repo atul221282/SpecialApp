3d9bc1d2e63a2525a95278863c44a03900262942
Atul Chaudhary <atul221282@gmail.com>
2017-06-02 22:30:07 +0930

    created string filter

--- SpecialApp.Base/Rules/QueryableSearchFilter/StringFilter.cs
+++ SpecialApp.Base/Rules/QueryableSearchFilter/StringFilter.cs
@@ -3,21 +3,21 @@ using System.Collections.Generic;
 
 namespace SpecialApp.Base.Rules.QueryableSearchFilter
 {
-    //public class StringFilter<T> : IPermitSearchFilter<T>
-    //{
-    //    private Tuple<string, Func<string, IEnumerable<T>>> p;
+    public class StringFilter : IListFIlter
+    {
+        private readonly Action action;
+        private readonly string value;
 
-    //    public StringFilter(Tuple<string, Func<string, IEnumerable<T>>> p)
-    //    {
-    //        this.p = p;
-    //    }
+        public StringFilter(string value, Action action)
+        {
+            this.action = action;
+            this.value = value;
+        }
 
-    //    public IEnumerable<T> Execute(IEnumerable<T> listData)
-    //    {
-    //        if (p.Item1.IsNotNullOrWhiteSpace())
-    //            listData = p.Item2(p.Item1);
-
-    //        return listData;
-    //    }
-    //}
+        public void Execute()
+        {
+            if (this.value.IsNotNullOrWhiteSpace())
+                action();
+        }
+    }
 }
